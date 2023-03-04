424991dc49e2219882b3e393691f39ca8a08f5f5
Omar Hamdash <ohamdash@hotmail.com>
2023-03-04 18:20:57 +0300

    added backend files

---
+++ gobooking/src/main/java/com/api/gobooking/GobookingApplication.java
@@ -0,0 +1,13 @@
+package com.api.gobooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GobookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GobookingApplication.class, args);
+	}
+
+}
