d8d636db92aa391431f7dd3aace229b2bdd4f045
MustafaYetgin <mustafa.yetgin@bilkent.edu.tr>
2023-05-22 21:20:49 +0300

    Booking Layers

---
+++ gobooking/src/main/java/com/api/gobooking/booking/BookingController.java
@@ -0,0 +1,80 @@
+package com.api.gobooking.booking;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("gobooking/bookings")
+public class BookingController {
+
+    private BookingService bookingService;
+
+    @Autowired
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createBooking(@RequestBody Booking booking) {
+        boolean created = bookingService.createBooking(booking);
+        if (created) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("Booking created successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Booking with the same ID already exists");
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateBooking(@PathVariable int id, @RequestBody Booking booking) {
+        booking.setBooking_id(id);
+        boolean updated = bookingService.updateBooking(booking);
+        if (updated) {
+            return ResponseEntity.status(HttpStatus.OK).body("Booking updated successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Booking not found");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteBooking(@PathVariable int id) {
+        boolean deleted = bookingService.deleteBookingById(id);
+        if (deleted) {
+            return ResponseEntity.status(HttpStatus.OK).body("Booking deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Booking not found");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Booking> getBookingById(@PathVariable int id) {
+        Booking booking = bookingService.findBookingById(id);
+        if (booking != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(booking);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @GetMapping("/thesameBooker's/{booker_id}")
+    public ResponseEntity<List<Booking>> getAllBookingByBookerId(@PathVariable int booker_id) {
+        List<Booking> bookings = bookingService.findAllBookingByBookerId(booker_id);
+        return ResponseEntity.status(HttpStatus.OK).body(bookings);
+    }
+    @GetMapping("/thesameProperty's/{property_id}")
+    public ResponseEntity<List<Booking>> getAllBookingByPropertyId(@PathVariable int property_id) {
+        List<Booking> bookings = bookingService.findAllBookingByBookerId(property_id);
+        return ResponseEntity.status(HttpStatus.OK).body(bookings);
+    }
+
+
+
+    @GetMapping
+    public ResponseEntity<List<Booking>> getAllBookings() {
+        List<Booking> bookings = bookingService.findAllBookings();
+        return ResponseEntity.status(HttpStatus.OK).body(bookings);
+    }
+}
